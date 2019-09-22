c15055b0f2e4110a7d1a03e791b1e908459a8d81
Petar Simic <simic.petar@outlook.com>
2019-09-23 00:06:17 +0200

    Add project files.

---
+++ Crowbar/Convert.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Crowbar
+{
+    /// <summary>
+    /// Interaction logic for Convert.xaml
+    /// </summary>
+    public partial class Convert : Window
+    {
+        public Convert()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportExcel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Folder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SheetName_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Crowbar/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Crowbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertPDF_Click(object sender, RoutedEventArgs e)
+        {
+            Convert convert = new Convert();
+            convert.ShowDialog();
+        }
+
+        private void CalculateSum_Click(object sender, RoutedEventArgs e)
+        {
+            Sum sum = new Sum();
+            sum.ShowDialog();
+        }
+
+        private void Sign_Click(object sender, RoutedEventArgs e)
+        {
+            Sign sign = new Sign();
+            sign.ShowDialog();
+        }
+    }
+}
