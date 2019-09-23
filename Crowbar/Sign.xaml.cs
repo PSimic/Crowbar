b1d3b48c8dc17f9048da43293ce13ab347482a4c
Petar Simic <simic.petar@outlook.com>
2019-09-23 10:29:44 +0200

    Tool za potpisivanje odradjen

--- Crowbar/Sign.xaml.cs
+++ Crowbar/Sign.xaml.cs
@@ -19,9 +19,55 @@ namespace Crowbar
     /// </summary>
     public partial class Sign : Window
     {
+        List<string> listOfFilesPath = new List<string>();
+        string signatureLocatoin = string.Empty;
+        string folderLocation = string.Empty;
+
         public Sign()
         {
             InitializeComponent();
+        }
+
+        private void ImportPDF_Click(object sender, RoutedEventArgs e)
+        {
+            listOfFilesPath.Clear();
+            listBox.Items.Clear();
+            ImportExport.ImportFiles(out listOfFilesPath, "PDF files (*.pdf)|*.pdf");
+
+            listBox.Items.Add("Broj ucitanih fajlova: " + listOfFilesPath.Count());
+        }
+
+        private void ImportSign_Click(object sender, RoutedEventArgs e)
+        {
+            ImportExport.ImportFile(out signatureLocatoin, "PNG file (*.png)|*.png");
+
+            listBox.Items.Add("Lokacija ucitanog potpisa: " + signatureLocatoin);
+        }
+
+        private void Folder_Click(object sender, RoutedEventArgs e)
+        {
+            ImportExport.ExportFolder(out folderLocation);
+
+            listBox.Items.Add("Putanja foldera: " + folderLocation);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string errors = string.Empty;
+                Tools.SignPDF(listOfFilesPath, folderLocation, signatureLocatoin, out errors);
+
+                if (!errors.Equals(string.Empty))
+                    listBox.Items.Add(errors);
+
+                listBox.Items.Add("--- Operacija uspesno zavrsena. ---");
+            }
+            catch (Exception ex)
+            {
+                listBox.Items.Clear();
+                listBox.Items.Add(ex.Message);
+            }
         }
     }
 }
---
+++ Crowbar/Tools.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Crowbar
+{
+    public class Tools
+    {
+        public static void SignPDF(List<string> filesPaths, string exportFolder, string signatureLocation, out string errors)
+        {
+            if (filesPaths.Count == 0)
+                throw new Exception("Nisu uneti PDF fajlovi.");
+
+            if (exportFolder.Equals(string.Empty))
+                throw new Exception("Nije selektovana putanja ya potpisane PDF fajlove.");
+
+            if (signatureLocation.Equals(string.Empty))
+                throw new Exception("Nije unet potpis.");
+
+            errors = string.Empty;
+
+            foreach (var file in filesPaths)
+            {
+                try
+                {
+                    using (Stream inputPdfStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (Stream inputImageStream = new FileStream(exportFolder, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (Stream outputPdfStream = new FileStream($@"{exportFolder}\{FileName(file)}.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        var reader = new PdfReader(inputPdfStream);
+                        var stamper = new PdfStamper(reader, outputPdfStream);
+                        var pdfContentByte = stamper.GetOverContent(1);
+
+                        reader.SelectPages("1");
+
+                        Image image = Image.GetInstance(inputImageStream);
+                        image.ScaleAbsoluteHeight(65);
+                        image.ScaleAbsoluteWidth(155);
+
+                        image.SetAbsolutePosition(80, 80);
+                        pdfContentByte.AddImage(image);
+                        stamper.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errors += ex.Message + $" '{FileName(file)}'" + "\n";
+                }
+            }
+        }
+
+        private static string FileName(string filePath)
+        {
+            string[] exportPath = filePath.Split('\\');
+
+            return exportPath[exportPath.Length - 1].Split('.')[0];
+        }
+    }
+}
